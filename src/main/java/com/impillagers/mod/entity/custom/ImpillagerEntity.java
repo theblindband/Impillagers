6174b5a9593b9b43d5a7ecbd7f2b4af4a070366b
thomas-nurse <thomas.p.nurse@gmail.com>
2025-02-03 20:24:09 +0000

    Added Dung Block Texture Updated Dung Ball Texture Slowed Impillagers

--- src/main/java/com/impillagers/mod/entity/custom/ImpillagerEntity.java
+++ src/main/java/com/impillagers/mod/entity/custom/ImpillagerEntity.java
@@ -47,7 +47,7 @@ public class ImpillagerEntity extends VillagerEntity {
     public static DefaultAttributeContainer.Builder createVillagerAttributes() {
         return MobEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 8)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.75)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 48.0);
     }
