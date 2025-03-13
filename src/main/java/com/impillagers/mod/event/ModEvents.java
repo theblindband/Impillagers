73654a9536c43a263d1f416e245b1b132f59c276
thomas-nurse <thomas.p.nurse@gmail.com>
2025-03-13 08:49:51 +0000

    Added check to ensure the Smelly Running is only added on the server

--- src/main/java/com/impillagers/mod/event/ModEvents.java
+++ src/main/java/com/impillagers/mod/event/ModEvents.java
@@ -14,17 +14,19 @@ public class ModEvents {
     public static void registerModEvents() {
 
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
-                    if (entity instanceof MobEntity mobEntity) {
-                        if (!entity.getType().isIn(ModTags.EntityTypes.IGNORE_SMELLY)) {
-                            if (mobEntity instanceof PathAwareEntity pathAwareEntity) {
-                                ((MobEntityAccessor) mobEntity).getGoalSelector().add(3, new FleeEntityGoal<>(pathAwareEntity, LivingEntity.class, 6.0F, 1.0, 1.5, new SmellyPredicate()));
-                            }
+            if (!world.isClient) {
+                if (entity instanceof MobEntity mobEntity) {
+                    if (!entity.getType().isIn(ModTags.EntityTypes.IGNORE_SMELLY)) {
+                        if (mobEntity instanceof PathAwareEntity pathAwareEntity) {
+                            ((MobEntityAccessor) mobEntity).getGoalSelector().add(3, new FleeEntityGoal<>(pathAwareEntity, LivingEntity.class, 6.0F, 1.0, 1.5, new SmellyPredicate()));
                         }
                     }
                 }
-        );
+            }
+        });
     }
 }
+
 
 
 
