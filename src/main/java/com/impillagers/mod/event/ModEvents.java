5afb75e729cce1d4cdb36bb09175104f001231b5
thomas-nurse <thomas.p.nurse@gmail.com>
2025-03-17 06:15:43 +0000

    Fixed Frog Mask Overlay getting stuck on when switching worlds & respawning

--- src/main/java/com/impillagers/mod/event/ModEvents.java
+++ src/main/java/com/impillagers/mod/event/ModEvents.java
@@ -2,12 +2,17 @@ package com.impillagers.mod.event;
 
 import com.impillagers.mod.mixin.MobEntityAccessor;
 import com.impillagers.mod.predicate.SmellyPredicate;
+import com.impillagers.mod.util.HudOverlayOpacityPayload;
 import com.impillagers.mod.util.ModTags;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.FleeEntityGoal;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class ModEvents {
 
@@ -22,6 +27,19 @@ public class ModEvents {
                         }
                     }
                 }
+            }
+        });
+
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            ServerPlayerEntity player = handler.getPlayer();
+            if (!player.getEntityWorld().isClient()) {
+                ServerPlayNetworking.send(player, new HudOverlayOpacityPayload(0));
+            }
+        });
+
+        ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
+            if (!newPlayer.getEntityWorld().isClient()) {
+                ServerPlayNetworking.send(newPlayer, new HudOverlayOpacityPayload(0));
             }
         });
     }
