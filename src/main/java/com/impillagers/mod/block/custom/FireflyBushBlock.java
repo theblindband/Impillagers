d2d55292c2d3aaabfc84045b9e35e3725182883c
thomas-nurse <thomas.p.nurse@gmail.com>
2025-03-17 15:58:32 +0000

    Firefly Bush Rewrite Fixed all weird behaviour Bushes now light properly on the day/night cycle Right click now scares off fireflies when using no item / random item Right click captures fireflies with bottle Right click functions now work based on the lit state of the bush Bushes only light up at less than 7 light level Fixed server desync in blockstates

--- src/main/java/com/impillagers/mod/block/custom/FireflyBushBlock.java
+++ src/main/java/com/impillagers/mod/block/custom/FireflyBushBlock.java
@@ -2,11 +2,9 @@ package com.impillagers.mod.block.custom;
 
 import com.impillagers.mod.item.ModItems;
 import com.impillagers.mod.particle.ModParticleTypes;
-import com.impillagers.mod.sounds.ModSoundEvents;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
@@ -14,6 +12,7 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemActionResult;
@@ -23,26 +22,27 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.LightType;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
 
-public class FireflyBushBlock extends PlantBlock implements Fertilizable {
+public class FireflyBushBlock extends PlantBlock {
+    public static final BooleanProperty LIT = Properties.LIT;
+    public static final IntProperty COOLDOWN = IntProperty.of("cooldown", 0, 10);
     public static final MapCodec<DeadBushBlock> CODEC = createCodec(DeadBushBlock::new);
     protected static final VoxelShape SHAPE = Block.createCuboidShape(2.0, 0.0, 2.0, 14.0, 13.0, 14.0);
-    public static final BooleanProperty LIT = Properties.LIT;
-    public static final BooleanProperty USED = BooleanProperty.of("used");
-    private int timeToRelight = 0;
+
+    public FireflyBushBlock(AbstractBlock.Settings settings) {
+        super(settings);
+        this.setDefaultState(this.getDefaultState().with(LIT, false).with(COOLDOWN, 0));
+    }
 
     @Override
     public MapCodec<DeadBushBlock> getCodec() {
         return CODEC;
     }
 
-    public FireflyBushBlock(AbstractBlock.Settings settings) {
-        super(settings);
-        this.setDefaultState(this.getDefaultState().with(LIT, Boolean.FALSE));
-        this.setDefaultState(this.getDefaultState().with(USED, Boolean.FALSE));
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(LIT, COOLDOWN);
     }
 
     @Override
@@ -50,124 +50,117 @@ public class FireflyBushBlock extends PlantBlock implements Fertilizable {
         return SHAPE;
     }
 
-    @Override
-    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        int i = pos.getX();
-        int j = pos.getY();
-        int k = pos.getZ();
-        double d = (double) i + random.nextDouble();
-        double e = (double) j + 0.7;
-        double f = (double) k + random.nextDouble();
-        BlockPos.Mutable mutable = new BlockPos.Mutable();
-        BlockState currentState = world.getBlockState(pos);
-        boolean used = state.get(USED);
-        //Might be a better idea to use the lit state instead
+    private boolean shouldBushBeLit(World world, BlockPos pos) {
+        return !world.isDay() && world.getLightLevel(pos) < 7;
+    }
 
-        //If lit is true then emits particles
-        if (!used) {
-            if (world.getLightLevel(LightType.BLOCK, pos) < 7 && world.getTimeOfDay() >= 13000 && world.getTimeOfDay() <= 23000) {
-                BlockState newState = currentState.with(Properties.LIT, true);
-                world.setBlockState(pos, newState);
-                world.playSound(pos.getX(), pos.getY(), pos.getZ(), ModSoundEvents.FIREFLY_BUSH, SoundCategory.AMBIENT, 1.0F, 1.0F, false);
-                for (int l = 0; l < 1; l++) {
-                    mutable.set(i + MathHelper.nextInt(random, -10, 10), j + random.nextInt(10), k + MathHelper.nextInt(random, -10, 10));
-                    BlockState blockState = world.getBlockState(mutable);
-                    if (!blockState.isFullCube(world, mutable)) {
-                        world.addParticle(
-                                ModParticleTypes.FIREFLY,
-                                (double) mutable.getX() + random.nextDouble(),
-                                (double) mutable.getY() + random.nextDouble(),
-                                (double) mutable.getZ() + random.nextDouble(),
-                                0.0,
-                                0.0,
-                                0.0
-                        );
-                    }
-                }
+
+    private void updateBushLightState(BlockState state, World world, BlockPos pos) {
+        if (state.get(COOLDOWN) == 0) {
+            boolean lit = shouldBushBeLit(world, pos);
+            world.setBlockState(pos, state.with(LIT, lit), Block.NOTIFY_ALL);
+        }
+    }
+
+    private void scheduleNextTick(World world, BlockPos pos, Random random) {
+        int delay = MathHelper.nextInt(random, 150, 300);
+        world.scheduleBlockTick(pos, this, delay);
+    }
+
+    private void scheduleCooldownTick(World world, BlockPos pos) {
+        world.scheduleBlockTick(pos, this, 120);
+    }
+
+    @Override
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        if (!world.isClient()) {
+            if (state.get(COOLDOWN) == 0) {
+                scheduleNextTick(world, pos, world.getRandom());
             } else {
-                BlockState newState = currentState.with(Properties.LIT, false);
-                world.setBlockState(pos, newState);
+                scheduleCooldownTick(world, pos);
             }
-        //If lit is false then check timer for relight
-            //If timer is 0 then lit is true
+        }
+        super.onBlockAdded(state, world, pos, oldState, notify);
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (state.get(COOLDOWN) > 0) {
+            int current = state.get(COOLDOWN);
+            int next = current - 1;
+            if (next <= 0) {
+                BlockState newState = state.with(COOLDOWN, 0).with(LIT, shouldBushBeLit(world, pos));
+                world.setBlockState(pos, newState, Block.NOTIFY_ALL);
+                scheduleNextTick(world, pos, random);
+            } else {
+                BlockState newState = state.with(COOLDOWN, next).with(LIT, false);
+                world.setBlockState(pos, newState, Block.NOTIFY_ALL);
+                scheduleCooldownTick(world, pos);
+            }
         } else {
-            if (timeToRelight > 0) {
-                timeToRelight = timeToRelight - 1;
-            } else { world.setBlockState(pos, state.with(USED, false));}
+            updateBushLightState(state, world, pos);
+            scheduleNextTick(world, pos, random);
         }
     }
 
     @Override
-    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state) {
-        return true;
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (state.get(FireflyBushBlock.LIT)) {
+            int i = pos.getX();
+            int j = pos.getY();
+            int k = pos.getZ();
+            BlockPos.Mutable mutable = new BlockPos.Mutable();
+
+            for (int l = 0; l < 1; l++) {
+                mutable.set(i + MathHelper.nextInt(random, -10, 10), j + random.nextInt(10), k + MathHelper.nextInt(random, -10, 10));
+                BlockState blockState = world.getBlockState(mutable);
+                if (!blockState.isFullCube(world, mutable)) {
+                    world.addParticle(
+                            ModParticleTypes.FIREFLY,
+                            mutable.getX() + random.nextDouble(),
+                            mutable.getY() + random.nextDouble(),
+                            mutable.getZ() + random.nextDouble(),
+                            0.0,
+                            0.0,
+                            0.0
+                    );
+                }
+            }
+        }
     }
 
-    @Override
-    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
-        return true;
-    }
-
-    @Override
-    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
-        dropStack(world, pos, new ItemStack(this));
-    }
-
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(LIT, USED);
-    }
 
     @Override
     protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-            {
-                if (!world.isClient()) {
-                    //If lit is true then you can use jar on the bush and set timer to XXXX
-                    boolean used = state.get(USED);
-                    if (!used) {
-                        ItemStack heldItem = player.getStackInHand(hand);
-
-                        Item myItem = Items.GLASS_BOTTLE;
-                        Item newItem = ModItems.FIREFLY_BOTTLE;
-
-                        if (heldItem.getItem() == myItem) {
-                            heldItem.decrement(1);
-
-                            ItemStack newItemStack = new ItemStack(newItem, 1);
-                            player.giveItemStack(newItemStack);
-
-                            world.setBlockState(pos, state.with(USED, true));
-                            timeToRelight = 10000;
-                            world.playSound(
-                                    null,
-                                    pos.getX(),
-                                    pos.getY(),
-                                    pos.getZ(),
-                                    SoundEvents.BLOCK_BEEHIVE_ENTER,
-                                    SoundCategory.NEUTRAL,
-                                    0.75F,
-                                    0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F)
-                            );
-                            return ItemActionResult.CONSUME;
-                        } else {
-                            world.setBlockState(pos, state.with(USED, true));
-                            timeToRelight = 10000;
-                            world.playSound(
-                                    null,
-                                    pos.getX(),
-                                    pos.getY(),
-                                    pos.getZ(),
-                                    SoundEvents.ENTITY_BEE_POLLINATE,
-                                    SoundCategory.NEUTRAL,
-                                    0.8F,
-                                    0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F)
-                            );
-                            return ItemActionResult.SUCCESS;
-                        }
-                    }
+        if (!world.isClient()) {
+            if (state.get(LIT)) {
+                world.setBlockState(pos, state.with(LIT, false), Block.NOTIFY_ALL);
+                ItemStack heldItem = player.getStackInHand(hand);
+                if (heldItem.getItem() == Items.GLASS_BOTTLE) {
+                    heldItem.decrement(1);
+                    ItemStack newItemStack = new ItemStack(ModItems.FIREFLY_BOTTLE, 1);
+                    player.giveItemStack(newItemStack);
+                    world.playSound(null,
+                            pos.getX(), pos.getY(), pos.getZ(),
+                            SoundEvents.BLOCK_BEEHIVE_ENTER,
+                            SoundCategory.NEUTRAL,
+                            0.75F,
+                            0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
+                    BlockState newState = state.with(LIT, false).with(COOLDOWN, 10);
+                    world.setBlockState(pos, newState, Block.NOTIFY_ALL);
+                    scheduleCooldownTick(world, pos);
+                    return ItemActionResult.CONSUME;
+                } else {
+                    world.playSound(null,
+                            pos.getX(), pos.getY(), pos.getZ(),
+                            SoundEvents.ENTITY_BEE_POLLINATE,
+                            SoundCategory.NEUTRAL,
+                            0.75F,
+                            0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
+                    return ItemActionResult.SUCCESS;
                 }
-
             }
-        return ItemActionResult.FAIL;
+        }
+        return ItemActionResult.SUCCESS;
     }
-
 }
