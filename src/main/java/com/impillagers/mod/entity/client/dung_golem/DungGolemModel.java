f31637e018001c94f3960714acffa8d6d21d6951
thomas-nurse <thomas.p.nurse@gmail.com>
2025-04-08 09:43:27 +0100

    Adjusted Dung Golem Model

--- src/main/java/com/impillagers/mod/entity/client/dung_golem/DungGolemModel.java
+++ src/main/java/com/impillagers/mod/entity/client/dung_golem/DungGolemModel.java
@@ -11,23 +11,24 @@ import net.minecraft.util.Identifier;
 
 public class DungGolemModel <T extends DungGolemEntity> extends SinglePartEntityModel<T> {
     public static final EntityModelLayer DUNG_GOLEM = new EntityModelLayer(Identifier.of(Impillagers.MOD_ID, "dung_golem"), "main");
-    private final ModelPart dung_golem;
+
+    private final ModelPart root;
 
     public DungGolemModel(ModelPart root) {
-        this.dung_golem = root.getChild("dung_golem");
-        ModelPart body = this.dung_golem.getChild("body");
+        this.root = root.getChild("root");
     }
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
         ModelPartData modelPartData = modelData.getRoot();
-        ModelPartData dung_golem = modelPartData.addChild("dung_golem", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+        ModelPartData root = modelPartData.addChild("root", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-        ModelPartData body = dung_golem.addChild("body", ModelPartBuilder.create().uv(0, 0).cuboid(-5.0F, -9.0F, -3.0F, 10.0F, 6.0F, 6.0F, new Dilation(0.0F))
-                .uv(28, 18).cuboid(-1.0F, -5.0F, -4.0F, 2.0F, 3.0F, 1.0F, new Dilation(0.0F))
-                .uv(0, 12).cuboid(-8.0F, -13.0F, 0.0F, 16.0F, 6.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData body = root.addChild("body", ModelPartBuilder.create().uv(0, 0).cuboid(-5.0F, -3.0F, -3.0F, 10.0F, 6.0F, 6.0F, new Dilation(0.0F))
+                .uv(0, 12).cuboid(-8.0F, -7.0F, 0.0F, 16.0F, 6.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, -6.0F, 0.0F));
 
-        ModelPartData flower = body.addChild("flower", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData nose = body.addChild("nose", ModelPartBuilder.create().uv(28, 18).cuboid(-1.0F, 0.0F, -1.0F, 2.0F, 3.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 1.0F, -3.0F));
+
+        ModelPartData flower = body.addChild("flower", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 6.0F, 0.0F));
 
         ModelPartData stem2_r1 = flower.addChild("stem2_r1", ModelPartBuilder.create().uv(8, 26).cuboid(-1.0F, -0.5F, 0.0F, 2.0F, 1.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-5.051F, -9.177F, -2.0669F, 0.0F, -1.5708F, -0.7854F));
 
@@ -35,19 +36,19 @@ public class DungGolemModel <T extends DungGolemEntity> extends SinglePartEntity
 
         ModelPartData head_r1 = flower.addChild("head_r1", ModelPartBuilder.create().uv(28, 22).cuboid(-1.4419F, -1.5301F, 0.0F, 3.0F, 3.0F, 0.0F, new Dilation(0.0F)), ModelTransform.of(-5.0581F, -9.2699F, -3.075F, 0.0F, 0.0F, 0.9599F));
 
-        ModelPartData legs = dung_golem.addChild("legs", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
-
-        ModelPartData rightLeg = legs.addChild("rightLeg", ModelPartBuilder.create().uv(12, 24).cuboid(-1.0F, 0.0F, -1.0F, 2.0F, 3.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(3.0F, -3.0F, 0.0F));
-
-        ModelPartData leftLeg = legs.addChild("leftLeg", ModelPartBuilder.create().uv(20, 24).cuboid(-1.0F, 0.0F, -1.0F, 2.0F, 3.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(-3.0F, -3.0F, 0.0F));
-
-        ModelPartData arms = dung_golem.addChild("arms", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, -6.0F, 0.0F));
+        ModelPartData arms = body.addChild("arms", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
         ModelPartData leftArm = arms.addChild("leftArm", ModelPartBuilder.create().uv(12, 18).cuboid(0.0F, 0.0F, -1.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F))
                 .uv(0, 18).cuboid(-0.5F, 2.0F, -1.5F, 3.0F, 4.0F, 3.0F, new Dilation(-0.3F)), ModelTransform.pivot(5.0F, 0.0F, 0.0F));
 
         ModelPartData rightArm = arms.addChild("rightArm", ModelPartBuilder.create().uv(20, 18).cuboid(-2.0F, 0.0F, -1.0F, 2.0F, 4.0F, 2.0F, new Dilation(0.0F))
                 .uv(0, 25).cuboid(-2.0F, 3.0F, -1.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.1F)), ModelTransform.pivot(-5.0F, 0.0F, 0.0F));
+
+        ModelPartData legs = body.addChild("legs", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 1.0F, 0.0F));
+
+        ModelPartData rightLeg = legs.addChild("rightLeg", ModelPartBuilder.create().uv(12, 24).cuboid(-1.0F, 0.0F, -1.0F, 2.0F, 3.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(3.0F, 2.0F, 0.0F));
+
+        ModelPartData leftLeg = legs.addChild("leftLeg", ModelPartBuilder.create().uv(20, 24).cuboid(-1.0F, 0.0F, -1.0F, 2.0F, 3.0F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(-3.0F, 2.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 64);
     }
 
@@ -62,11 +63,11 @@ public class DungGolemModel <T extends DungGolemEntity> extends SinglePartEntity
 
     @Override
     public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, int color) {
-        dung_golem.render(matrices, vertexConsumer, light, overlay, color);
+        root.render(matrices, vertexConsumer, light, overlay, color);
     }
 
     @Override
     public ModelPart getPart() {
-        return dung_golem;
+        return root;
     }
 }
