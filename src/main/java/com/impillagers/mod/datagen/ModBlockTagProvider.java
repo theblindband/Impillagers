1f32a0707a15886be2b22a42416200e19ca51d4c
2100874 <2100874@student.uwtsd.ac.uk>
2025-04-11 12:16:58 +0100

    Textures and Structure Changes Suspicious Dirt and Fertile Blocks now have textures. Reworked Swamp Ruins to use Suspicious Dirt instead of gravel.
    Minor advancement tweaks.

--- src/main/java/com/impillagers/mod/datagen/ModBlockTagProvider.java
+++ src/main/java/com/impillagers/mod/datagen/ModBlockTagProvider.java
@@ -4,6 +4,7 @@ import com.impillagers.mod.block.ModBlocks;
 import com.impillagers.mod.util.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 
