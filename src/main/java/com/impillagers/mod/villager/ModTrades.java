ad20a4801469ef44abe611792cdbc228a43a1a52
2100874 <2100874@student.uwtsd.ac.uk>
2025-05-19 23:47:32 +0100

    Village Additions
    Temple now has a basement
    Banker building now uses the safe inside it
    
    Professions have had their blocks adjusted
    Trades have been added for all professions, though some are placeholder for testing purposes.

--- src/main/java/com/impillagers/mod/villager/ModTrades.java
+++ src/main/java/com/impillagers/mod/villager/ModTrades.java
@@ -23,7 +23,6 @@ public class ModTrades {
                 new TradeData(Blocks.RAW_GOLD_BLOCK, 2, ModItems.GOLD_COIN, 8, 16, 2, 0.04f),
                 new TradeData(Blocks.RAW_GOLD_BLOCK, 2, ModItems.FAKE_GOLD_COIN, 8, 16, 2, 0.04f)
         );
-
         // Banker - Level 2: Gold Ingot Trades
         registerTrades(ModProfessions.BANKER, 2,
                 new TradeData(Items.GOLD_INGOT, 1, ModItems.GOLD_COIN, 1, 16, 2, 0.04f),
@@ -31,7 +30,6 @@ public class ModTrades {
                 new TradeData(Blocks.GOLD_BLOCK, 1, ModItems.GOLD_COIN, 8, 16, 2, 0.04f),
                 new TradeData(Items.GOLD_BLOCK, 1, ModItems.FAKE_GOLD_COIN, 8, 16, 2, 0.04f)
         );
-
         // Banker - Level 3: Gold Trades (Armor and Tools)
         registerTrades(ModProfessions.BANKER, 3,
                 new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_HELMET, 1, 16, 2, 0.04f),
@@ -44,13 +42,11 @@ public class ModTrades {
                 new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_SHOVEL, 1, 16, 2, 0.04f),
                 new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_SWORD, 1, 16, 2, 0.04f)
         );
-
         // Banker - Level 4: Rare Gold Items
         registerTrades(ModProfessions.BANKER, 4,
                 new TradeData(ModItems.GOLD_COIN, 48, Blocks.BELL, 1, 16, 2, 0.04f),
                 new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_APPLE, 1, 16, 2, 0.04f)
         );
-
         // Banker - Level 5: Fake Coins to Real Coins
         registerTrades(ModProfessions.BANKER, 5,
                 new TradeData(ModItems.FAKE_GOLD_COIN, 16, ModItems.GOLD_COIN, 1, 16, 2, 0.04f)
@@ -65,13 +61,11 @@ public class ModTrades {
                 new TradeData(Items.MUSIC_DISC_13, 1, ModItems.GOLD_COIN, 8, 16, 6, 0.04f),
                 new TradeData(ModItems.GOLD_COIN, 16, Items.MUSIC_DISC_13, 1, 16, 6, 0.04f)
         );
-
         // Musician - Level 2: Creeper Discs
         registerTrades(ModProfessions.MUSICIAN, 2,
                 new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_11, 1, 16, 6, 0.04f),
                 new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_CHIRP, 1, 16, 6, 0.04f)
         );
-
         // Musician - Level 3: Structure Discs
         registerTrades(ModProfessions.MUSICIAN, 3,
                 new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_CREATOR, 1, 16, 6, 0.04f),
@@ -81,7 +75,6 @@ public class ModTrades {
                 new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_OTHERSIDE, 1, 16, 6, 0.04f),
                 new TradeData(ModItems.GOLD_COIN, 8, Items.DISC_FRAGMENT_5, 1, 16, 6, 0.04f)
         );
-
         // Musician - Level 4: Noteblock/Jukebox
         registerTrades(ModProfessions.MUSICIAN, 4,
                 new TradeData(Blocks.NOTE_BLOCK, 4, ModItems.FAKE_GOLD_COIN, 2, 16, 6, 0.04f),
@@ -89,7 +82,6 @@ public class ModTrades {
                 new TradeData(ModItems.GOLD_COIN, 2, Blocks.NOTE_BLOCK, 2, 16, 6, 0.04f),
                 new TradeData(ModItems.GOLD_COIN, 16, Blocks.JUKEBOX, 1, 16, 6, 0.04f)
         );
-
         // Musician - Level 5: Goat Horns
         registerTrades(ModProfessions.MUSICIAN, 5,
                 new TradeData(ModItems.GOLD_COIN, 16, Items.GOAT_HORN, 1, 16, 6, 0.04f)
@@ -104,7 +96,6 @@ public class ModTrades {
                 new TradeData(Blocks.COARSE_DIRT, 16, ModItems.FAKE_GOLD_COIN, 2, 16, 6, 0.04f),
                 new TradeData(Blocks.ROOTED_DIRT, 16, ModItems.FAKE_GOLD_COIN, 3, 16, 6, 0.04f)
         );
-
         // Dung Collector - Level 2: Other Natural Shovel Blocks
         registerTrades(ModProfessions.DUNG_COLLECTOR, 2,
                 new TradeData(Blocks.SAND, 32, ModItems.GOLD_COIN, 1, 16, 6, 0.04f),
@@ -114,30 +105,68 @@ public class ModTrades {
                 new TradeData(Blocks.GRAVEL, 16, ModItems.FAKE_GOLD_COIN, 1, 16, 6, 0.04f),
                 new TradeData(Items.CLAY_BALL, 32, ModItems.FAKE_GOLD_COIN, 1, 16, 6, 0.04f)
         );
-
         // Dung Collector - Level 3: Mud
         registerTrades(ModProfessions.DUNG_COLLECTOR, 3,
                 new TradeData(ModItems.GOLD_COIN, 8, Blocks.MUD, 8, 16, 6, 0.04f),
                 new TradeData(ModItems.GOLD_COIN, 8, ModBlocks.SINKING_MUD, 4, 16, 6, 0.04f)
         );
-
         // Dung Collector - Level 4: Dung Balls
         registerTrades(ModProfessions.DUNG_COLLECTOR, 4,
                 new TradeData(ModItems.GOLD_COIN, 8, ModItems.DUNG_BALL, 6, 16, 6, 0.04f)
         );
-
         // Dung Collector - Level 5: Dung Blocks
         registerTrades(ModProfessions.DUNG_COLLECTOR, 5,
                 new TradeData(ModItems.GOLD_COIN, 16, ModBlocks.DUNG_BLOCK, 8, 16, 6, 0.04f)
         );
 
-        //Explosives Expert
-        //Level 1 - Gunpowder and Sand
-        //Level 2 - TNT
-        //Level 3 - Creeper Oil
-        //Level 4 - Enchantments
-        //Level 5 - End Crystals (Might be too strong to buy them from it)
+        // Explosives Expert - Level 1: Gunpowder and Sand
+        registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 1,
+                new TradeData(Items.GUNPOWDER, 16, ModItems.GOLD_COIN, 2, 14, 6, 0.04f),
+                new TradeData(Blocks.SAND, 16, ModItems.GOLD_COIN, 2, 14, 6, 0.04f),
+                new TradeData(Items.GUNPOWDER, 16, ModItems.FAKE_GOLD_COIN, 2, 14, 6, 0.04f),
+                new TradeData(Blocks.SAND, 16, ModItems.FAKE_GOLD_COIN, 2, 14, 6, 0.04f)
+        );
+        // Explosives Expert - Level 2: TNT
+        registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 2,
+                new TradeData(ModItems.GOLD_COIN, 6, Blocks.TNT, 2, 6, 6, 0.04f)
+        );
+        // Explosives Expert - Level 3: Creeper Oil
+        registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 3,
+                new TradeData(ModItems.GOLD_COIN, 10, ModItems.CREEPER_OIL, 1, 3, 6, 0.04f)
+        );
+        // Explosives Expert - Level 4: Enchantments
+        registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 4,
+                new TradeData(ModItems.GOLD_COIN, 64, Items.BOOK, 1, 1, 6, 0.04f)
+        );
+        // Explosives Expert - Level 5: End Crystal
+        registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 5,
+                new TradeData(ModItems.GOLD_COIN, 16, Items.END_CRYSTAL, 1, 4, 6, 0.04f)
+        );
 
+        // Druid - Level 1:
+        registerTrades(ModProfessions.DRUID, 1,
+                new TradeData(ModItems.GOLD_COIN, 64, ModBlocks.BELLADONNA, 1, 1, 6, 0.04f)
+        );
+
+        // Alchemist - Level 1:
+        registerTrades(ModProfessions.ALCHEMIST, 1,
+                new TradeData(ModItems.GOLD_COIN, 64, Items.GLASS_BOTTLE, 1, 1, 6, 0.04f)
+        );
+
+        // Occultist - Level 1:
+        registerTrades(ModProfessions.OCCULTIST, 1,
+                new TradeData(ModItems.GOLD_COIN, 64, Items.NETHER_WART, 1, 1, 6, 0.04f)
+        );
+
+        // Clan Leader - Level 1:
+        registerTrades(ModProfessions.CLAN_LEADER, 1,
+                new TradeData(ModItems.GOLD_COIN, 64, Items.GOLDEN_APPLE, 1, 1, 6, 0.04f)
+        );
+
+        // Guard - Level 1:
+        registerTrades(ModProfessions.GUARD, 1,
+                new TradeData(ModItems.GOLD_COIN, 64, Items.TRIDENT, 1, 1, 6, 0.04f)
+        );
     }
 
     private static void registerTrades(VillagerProfession profession, int level, TradeData... trades) {
--- src/main/java/com/impillagers/mod/villager/professions/ModProfessions.java
+++ src/main/java/com/impillagers/mod/villager/professions/ModProfessions.java
@@ -21,7 +21,7 @@ public class ModProfessions {
 
     //Safe
     public static final VillagerProfession BANKER =
-            createProfession("banker", Blocks.GOLD_BLOCK, SoundEvents.ENTITY_VILLAGER_WORK_LIBRARIAN);
+            createProfession("banker", ModBlocks.SAFE, SoundEvents.ENTITY_VILLAGER_WORK_TOOLSMITH);
 
     public static final VillagerProfession DUNG_COLLECTOR =
             createProfession("dung_collector", ModBlocks.DUNG_BLOCK, SoundEvents.BLOCK_MUD_HIT);
@@ -29,21 +29,16 @@ public class ModProfessions {
     public static final VillagerProfession MUSICIAN =
             createProfession("musician", Blocks.JUKEBOX, SoundEvents.ENTITY_VILLAGER_WORK_LIBRARIAN);
 
-    //Use either a Target Block or Anvil
-    public static final VillagerProfession GUARD =
-            createProfession("guard", Blocks.TARGET, SoundEvents.ITEM_SHIELD_BLOCK);
-
-    //We could make a custom block, but I like the idea of the imps having tnt lying around the buildings
-    //Depending on how much tnt we want in the building will decide on if it will be the workstation or not
     public static final VillagerProfession EXPLOSIVES_EXPERT =
             createProfession("explosives_expert", Blocks.TNT, SoundEvents.ENTITY_CREEPER_PRIMED);
 
+    //???
     public static final VillagerProfession DRUID =
-            createProfession("druid", ModBlocks.BELLADONNA, SoundEvents.ITEM_CROP_PLANT);
+            createProfession("druid", ModBlocks.FIREFLY_BOTTLE, SoundEvents.ITEM_CROP_PLANT);
 
     //Alchemy Table
     public static final VillagerProfession ALCHEMIST =
-            createProfession("alchemist", Blocks.CANDLE, SoundEvents.BLOCK_BREWING_STAND_BREW);
+            createProfession("alchemist", Blocks.WARPED_WART_BLOCK, SoundEvents.BLOCK_BREWING_STAND_BREW);
 
     //Altar
     public static final VillagerProfession OCCULTIST =
@@ -51,7 +46,10 @@ public class ModProfessions {
 
     //Throne or maybe the Bell since there can only be one of these per village
     public static final VillagerProfession CLAN_LEADER =
-            createProfession("clan_leader", Blocks.CHERRY_STAIRS, ModSoundEvents.IMPILLAGER_YES);
+            createProfession("clan_leader", ModBlocks.CHISELED_DUNG_BRICKS, ModSoundEvents.IMPILLAGER_YES);
+
+    public static final VillagerProfession GUARD =
+            createProfession("guard", Blocks.TARGET, SoundEvents.ITEM_SHIELD_BLOCK);
 
 
 
