610cd9d462750528eae58a0d102bfe543081e426
2100874 <2100874@student.uwtsd.ac.uk>
2025-05-21 17:13:17 +0100

    Rebalanced Imp Trades
    Organised and Rebalanced Impillager trades.

--- src/main/java/com/impillagers/mod/villager/ModTrades.java
+++ src/main/java/com/impillagers/mod/villager/ModTrades.java
@@ -16,156 +16,177 @@ public class ModTrades {
 
     public static void registerModTrades() {
 
+        /* -----BANKER----- */
         // Banker - Level 1: Raw Gold Trades
         registerTrades(ModProfessions.BANKER, 1,
-                new TradeData(Items.RAW_GOLD, 2, ModItems.GOLD_COIN, 1, 16, 2, 0.04f),
-                new TradeData(Items.GOLD_NUGGET, 2, ModItems.FAKE_GOLD_COIN, 1, 16, 2, 0.04f),
-                new TradeData(Blocks.RAW_GOLD_BLOCK, 2, ModItems.GOLD_COIN, 8, 16, 2, 0.04f),
-                new TradeData(Blocks.RAW_GOLD_BLOCK, 2, ModItems.FAKE_GOLD_COIN, 8, 16, 2, 0.04f)
+                new TradeData(Items.RAW_GOLD, 16, ModItems.GOLD_COIN, 1, 16, 2, 0.02f),
+                new TradeData(Items.RAW_GOLD, 16, ModItems.FAKE_GOLD_COIN, 1, 16, 2, 0.02f),
+                new TradeData(Items.RAW_GOLD_BLOCK, 2, ModItems.GOLD_COIN, 1, 16, 4, 0.02f),
+                new TradeData(Items.RAW_GOLD_BLOCK, 2, ModItems.FAKE_GOLD_COIN, 1, 16, 4, 0.02f)
         );
         // Banker - Level 2: Gold Ingot Trades
         registerTrades(ModProfessions.BANKER, 2,
-                new TradeData(Items.GOLD_INGOT, 1, ModItems.GOLD_COIN, 1, 16, 2, 0.04f),
-                new TradeData(Items.GOLD_INGOT, 1, ModItems.FAKE_GOLD_COIN, 1, 16, 2, 0.04f),
-                new TradeData(Blocks.GOLD_BLOCK, 1, ModItems.GOLD_COIN, 8, 16, 2, 0.04f),
-                new TradeData(Items.GOLD_BLOCK, 1, ModItems.FAKE_GOLD_COIN, 8, 16, 2, 0.04f)
+                new TradeData(Items.GOLD_INGOT, 16, ModItems.GOLD_COIN, 1, 12, 10, 0.02f),
+                new TradeData(Items.GOLD_INGOT, 16, ModItems.FAKE_GOLD_COIN, 1, 12, 10, 0.02f),
+                new TradeData(Items.GOLD_BLOCK, 2, ModItems.GOLD_COIN, 1, 12, 12, 0.02f),
+                new TradeData(Items.GOLD_BLOCK, 2, ModItems.FAKE_GOLD_COIN, 1, 12, 12, 0.02f)
         );
         // Banker - Level 3: Gold Trades (Armor and Tools)
         registerTrades(ModProfessions.BANKER, 3,
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_HELMET, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_CHESTPLATE, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_LEGGINGS, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_BOOTS, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_AXE, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_HOE, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_PICKAXE, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_SHOVEL, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_SWORD, 1, 16, 2, 0.04f)
+                //Armor
+                new TradeData(ModItems.GOLD_COIN, 5, Items.GOLDEN_HELMET, 1, 5, 5, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 9, Items.GOLDEN_CHESTPLATE, 1, 5, 5, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 7, Items.GOLDEN_LEGGINGS, 1, 5, 5, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 4, Items.GOLDEN_BOOTS, 1, 5, 5, 0.02f),
+                //Tools
+                new TradeData(ModItems.GOLD_COIN, 5, Items.GOLDEN_AXE, 1, 5, 5, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 3, Items.GOLDEN_HOE, 1, 5, 5, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 4, Items.GOLDEN_PICKAXE, 1, 5, 5, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 3, Items.GOLDEN_SHOVEL, 1, 5, 5, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 6, Items.GOLDEN_SWORD, 1, 5, 5, 0.02f)
         );
         // Banker - Level 4: Rare Gold Items
         registerTrades(ModProfessions.BANKER, 4,
-                new TradeData(ModItems.GOLD_COIN, 48, Blocks.BELL, 1, 16, 2, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.GOLDEN_APPLE, 1, 16, 2, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 36, Blocks.BELL, 1, 12, 15, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 15, Items.GOLDEN_APPLE, 1, 16, 12, 0.02f)
         );
         // Banker - Level 5: Fake Coins to Real Coins
         registerTrades(ModProfessions.BANKER, 5,
-                new TradeData(ModItems.FAKE_GOLD_COIN, 16, ModItems.GOLD_COIN, 1, 16, 2, 0.04f)
+                new TradeData(ModItems.FAKE_GOLD_COIN, 8, ModItems.GOLD_COIN, 1, 16, 30, 0.02f)
         );
 
+        /* -----MUSICIAN----- */
         // Musician - Level 1: Dungeon Discs
         registerTrades(ModProfessions.MUSICIAN, 1,
-                new TradeData(Items.MUSIC_DISC_CAT, 1, ModItems.FAKE_GOLD_COIN, 8, 16, 6, 0.04f),
-                new TradeData(Items.MUSIC_DISC_CAT, 1, ModItems.GOLD_COIN, 8, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 16, Items.MUSIC_DISC_CAT, 1, 16, 6, 0.04f),
-                new TradeData(Items.MUSIC_DISC_13, 1, ModItems.FAKE_GOLD_COIN, 8, 16, 6, 0.04f),
-                new TradeData(Items.MUSIC_DISC_13, 1, ModItems.GOLD_COIN, 8, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 16, Items.MUSIC_DISC_13, 1, 16, 6, 0.04f)
+                new TradeData(Items.MUSIC_DISC_CAT, 1, ModItems.GOLD_COIN, 6, 3, 6, 0.02f),
+                new TradeData(Items.MUSIC_DISC_CAT, 1, ModItems.FAKE_GOLD_COIN, 6, 3, 6, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 15, Items.MUSIC_DISC_CAT, 1, 3, 6, 0.02f),
+                new TradeData(Items.MUSIC_DISC_13, 1, ModItems.GOLD_COIN, 6, 3, 6, 0.02f),
+                new TradeData(Items.MUSIC_DISC_13, 1, ModItems.FAKE_GOLD_COIN, 6, 3, 6, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 15, Items.MUSIC_DISC_13, 1, 3, 6, 0.02f)
         );
-        // Musician - Level 2: Creeper Discs
+        // Musician - Level 2: Noteblock/Jukebox
         registerTrades(ModProfessions.MUSICIAN, 2,
-                new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_11, 1, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_CHIRP, 1, 16, 6, 0.04f)
+                new TradeData(Blocks.NOTE_BLOCK, 4, ModItems.GOLD_COIN, 2, 12, 8, 0.02f),
+                new TradeData(Blocks.NOTE_BLOCK, 4, ModItems.FAKE_GOLD_COIN, 2, 12, 8, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 12, Blocks.JUKEBOX, 1, 5, 10, 0.02f)
         );
-        // Musician - Level 3: Structure Discs
+        // Musician - Level 3: Creeper Discs
         registerTrades(ModProfessions.MUSICIAN, 3,
-                new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_CREATOR, 1, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 48, Items.MUSIC_DISC_CREATOR_MUSIC_BOX, 1, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 32, Items.MUSIC_DISC_PIGSTEP, 1, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_RELIC, 1, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 64, Items.MUSIC_DISC_OTHERSIDE, 1, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 8, Items.DISC_FRAGMENT_5, 1, 16, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_BLOCKS, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_CHIRP, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_FAR, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_MALL, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_MELLOHI, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_STAL, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_STRAD, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_WARD, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_11, 1, 3, 12, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 24, Items.MUSIC_DISC_WAIT, 1, 3, 12, 0.02f)
         );
-        // Musician - Level 4: Noteblock/Jukebox
+        // Musician - Level 4: Structure Discs
         registerTrades(ModProfessions.MUSICIAN, 4,
-                new TradeData(Blocks.NOTE_BLOCK, 4, ModItems.FAKE_GOLD_COIN, 2, 16, 6, 0.04f),
-                new TradeData(Blocks.NOTE_BLOCK, 4, ModItems.GOLD_COIN, 2, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 2, Blocks.NOTE_BLOCK, 2, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 16, Blocks.JUKEBOX, 1, 16, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 36, Items.MUSIC_DISC_OTHERSIDE, 1, 3, 15, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 4, Items.DISC_FRAGMENT_5, 1, 18, 5, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 36, Items.MUSIC_DISC_PIGSTEP, 1, 3, 20, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 28, Items.MUSIC_DISC_RELIC, 1, 3, 15, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 36, Items.MUSIC_DISC_CREATOR, 1, 3, 15, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 28, Items.MUSIC_DISC_CREATOR_MUSIC_BOX, 3, 16, 20, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 28, Items.MUSIC_DISC_PRECIPICE, 3, 16, 20, 0.02f)
         );
         // Musician - Level 5: Goat Horns
         registerTrades(ModProfessions.MUSICIAN, 5,
-                new TradeData(ModItems.GOLD_COIN, 16, Items.GOAT_HORN, 1, 16, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 15, Items.GOAT_HORN, 1, 3, 30, 0.02f)
         );
 
+        /* -----DUNG COLLECTOR----- */
         // Dung Collector - Level 1: Dirt Type Blocks
         registerTrades(ModProfessions.DUNG_COLLECTOR, 1,
-                new TradeData(Blocks.DIRT, 16, ModItems.GOLD_COIN, 1, 16, 6, 0.04f),
-                new TradeData(Blocks.COARSE_DIRT, 16, ModItems.GOLD_COIN, 2, 16, 6, 0.04f),
-                new TradeData(Blocks.ROOTED_DIRT, 16, ModItems.GOLD_COIN, 3, 16, 6, 0.04f),
-                new TradeData(Blocks.DIRT, 16, ModItems.FAKE_GOLD_COIN, 1, 16, 6, 0.04f),
-                new TradeData(Blocks.COARSE_DIRT, 16, ModItems.FAKE_GOLD_COIN, 2, 16, 6, 0.04f),
-                new TradeData(Blocks.ROOTED_DIRT, 16, ModItems.FAKE_GOLD_COIN, 3, 16, 6, 0.04f)
+                new TradeData(Blocks.DIRT, 12, ModItems.GOLD_COIN, 1, 16, 2, 0.02f),
+                new TradeData(Blocks.DIRT, 12, ModItems.FAKE_GOLD_COIN, 1, 16, 2, 0.02f),
+                new TradeData(Blocks.COARSE_DIRT, 12, ModItems.GOLD_COIN, 2, 16, 2, 0.02f),
+                new TradeData(Blocks.COARSE_DIRT, 12, ModItems.FAKE_GOLD_COIN, 2, 16, 2, 0.02f),
+                new TradeData(Blocks.ROOTED_DIRT, 12, ModItems.GOLD_COIN, 3, 16, 2, 0.02f),
+                new TradeData(Blocks.ROOTED_DIRT, 12, ModItems.FAKE_GOLD_COIN, 3, 16, 2, 0.02f)
         );
         // Dung Collector - Level 2: Other Natural Shovel Blocks
         registerTrades(ModProfessions.DUNG_COLLECTOR, 2,
-                new TradeData(Blocks.SAND, 32, ModItems.GOLD_COIN, 1, 16, 6, 0.04f),
-                new TradeData(Blocks.GRAVEL, 16, ModItems.GOLD_COIN, 1, 16, 6, 0.04f),
-                new TradeData(Items.CLAY_BALL, 32, ModItems.GOLD_COIN, 1, 16, 6, 0.04f),
-                new TradeData(Blocks.SAND, 32, ModItems.FAKE_GOLD_COIN, 1, 16, 6, 0.04f),
-                new TradeData(Blocks.GRAVEL, 16, ModItems.FAKE_GOLD_COIN, 1, 16, 6, 0.04f),
-                new TradeData(Items.CLAY_BALL, 32, ModItems.FAKE_GOLD_COIN, 1, 16, 6, 0.04f)
+                new TradeData(Blocks.SAND, 12, ModItems.GOLD_COIN, 1, 16, 10, 0.02f),
+                new TradeData(Blocks.SAND, 12, ModItems.FAKE_GOLD_COIN, 1, 16, 10, 0.02f),
+                new TradeData(Blocks.GRAVEL, 12, ModItems.GOLD_COIN, 1, 16, 10, 0.02f),
+                new TradeData(Blocks.GRAVEL, 12, ModItems.FAKE_GOLD_COIN, 1, 16, 10, 0.02f),
+                new TradeData(Items.CLAY_BALL, 24, ModItems.GOLD_COIN, 1, 16, 10, 0.02f),
+                new TradeData(Items.CLAY_BALL, 24, ModItems.FAKE_GOLD_COIN, 1, 16, 10, 0.02f)
         );
         // Dung Collector - Level 3: Mud
         registerTrades(ModProfessions.DUNG_COLLECTOR, 3,
-                new TradeData(ModItems.GOLD_COIN, 8, Blocks.MUD, 8, 16, 6, 0.04f),
-                new TradeData(ModItems.GOLD_COIN, 8, ModBlocks.SINKING_MUD, 4, 16, 6, 0.04f)
+                new TradeData(Blocks.MUD, 12, ModItems.GOLD_COIN, 1, 16, 10, 0.02f),
+                new TradeData(Blocks.MUD, 12, ModItems.FAKE_GOLD_COIN, 1, 16, 10, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 8, Blocks.MUD, 8, 12, 10, 0.02f),
+                new TradeData(ModItems.GOLD_COIN, 8, ModBlocks.SINKING_MUD, 4, 12, 10, 0.02f)
         );
         // Dung Collector - Level 4: Dung Balls
         registerTrades(ModProfessions.DUNG_COLLECTOR, 4,
-                new TradeData(ModItems.GOLD_COIN, 8, ModItems.DUNG_BALL, 6, 16, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 4, ModItems.DUNG_BALL, 12, 8, 15, 0.02f)
         );
         // Dung Collector - Level 5: Dung Blocks
         registerTrades(ModProfessions.DUNG_COLLECTOR, 5,
-                new TradeData(ModItems.GOLD_COIN, 16, ModBlocks.DUNG_BLOCK, 8, 16, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 4, ModBlocks.DUNG_BLOCK, 12, 8, 30, 0.02f)
         );
 
+        /* -----EXPLOSIVES EXPERT----- */
         // Explosives Expert - Level 1: Gunpowder and Sand
         registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 1,
-                new TradeData(Items.GUNPOWDER, 16, ModItems.GOLD_COIN, 2, 14, 6, 0.04f),
-                new TradeData(Blocks.SAND, 16, ModItems.GOLD_COIN, 2, 14, 6, 0.04f),
-                new TradeData(Items.GUNPOWDER, 16, ModItems.FAKE_GOLD_COIN, 2, 14, 6, 0.04f),
-                new TradeData(Blocks.SAND, 16, ModItems.FAKE_GOLD_COIN, 2, 14, 6, 0.04f)
+                new TradeData(Items.GUNPOWDER, 15, ModItems.GOLD_COIN, 1, 16, 2, 0.02f),
+                new TradeData(Items.GUNPOWDER, 15, ModItems.FAKE_GOLD_COIN, 1, 16, 2, 0.02f),
+                new TradeData(Blocks.SAND, 12, ModItems.GOLD_COIN, 1, 16, 3, 0.02f),
+                new TradeData(Blocks.SAND, 12, ModItems.FAKE_GOLD_COIN, 1, 16, 3, 0.02f)
         );
         // Explosives Expert - Level 2: TNT
         registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 2,
-                new TradeData(ModItems.GOLD_COIN, 6, Blocks.TNT, 2, 6, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 10, Blocks.TNT, 2, 5, 12, 0.02f)
         );
         // Explosives Expert - Level 3: Creeper Oil
         registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 3,
-                new TradeData(ModItems.GOLD_COIN, 10, ModItems.CREEPER_OIL, 1, 3, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 10, ModItems.CREEPER_OIL, 1, 3, 20, 0.02f)
         );
         // Explosives Expert - Level 4: Enchantments
         registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 4,
-                new TradeData(ModItems.GOLD_COIN, 64, Items.BOOK, 1, 1, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 64, Items.BOOK, 1, 1, 6, 0.02f)
         );
         // Explosives Expert - Level 5: End Crystal
         registerTrades(ModProfessions.EXPLOSIVES_EXPERT, 5,
-                new TradeData(ModItems.GOLD_COIN, 16, Items.END_CRYSTAL, 1, 4, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 36, Items.END_CRYSTAL, 1, 4, 30, 0.02f)
         );
 
+        /* -----DRUID----- */
         // Druid - Level 1:
         registerTrades(ModProfessions.DRUID, 1,
-                new TradeData(ModItems.GOLD_COIN, 64, ModBlocks.BELLADONNA, 1, 1, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 64, ModBlocks.BELLADONNA, 1, 1, 6, 0.02f)
         );
 
+        /* -----ALCHEMIST----- */
         // Alchemist - Level 1:
         registerTrades(ModProfessions.ALCHEMIST, 1,
-                new TradeData(ModItems.GOLD_COIN, 64, Items.GLASS_BOTTLE, 1, 1, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 64, Items.GLASS_BOTTLE, 1, 1, 6, 0.02f)
         );
 
+        /* -----OCCULTIST----- */
         // Occultist - Level 1:
         registerTrades(ModProfessions.OCCULTIST, 1,
-                new TradeData(ModItems.GOLD_COIN, 64, Items.NETHER_WART, 1, 1, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 64, Items.NETHER_WART, 1, 1, 6, 0.02f)
         );
 
+        /* -----CLAN LEADER----- */
         // Clan Leader - Level 1:
         registerTrades(ModProfessions.CLAN_LEADER, 1,
-                new TradeData(ModItems.GOLD_COIN, 64, Items.GOLDEN_APPLE, 1, 1, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 64, Items.GOLDEN_APPLE, 1, 1, 6, 0.02f)
         );
 
+        /* -----GUARD----- */
         // Guard - Level 1:
         registerTrades(ModProfessions.GUARD, 1,
-                new TradeData(ModItems.GOLD_COIN, 64, Items.TRIDENT, 1, 1, 6, 0.04f)
+                new TradeData(ModItems.GOLD_COIN, 64, Items.TRIDENT, 1, 1, 6, 0.02f)
         );
     }
 
