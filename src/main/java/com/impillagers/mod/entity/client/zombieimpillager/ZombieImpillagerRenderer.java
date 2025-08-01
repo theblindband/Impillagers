8598508fc9076676f6b1f152d2486ca32b47ddb4
thomas-nurse <thomas.p.nurse@gmail.com>
2025-08-01 13:42:12 +0100

    Removed Shadow method in ZombieImpillagerRenderer

--- src/main/java/com/impillagers/mod/entity/client/zombieimpillager/ZombieImpillagerRenderer.java
+++ src/main/java/com/impillagers/mod/entity/client/zombieimpillager/ZombieImpillagerRenderer.java
@@ -7,7 +7,7 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-//TODO: FIX
+
 @Environment(EnvType.CLIENT)
 public class ZombieImpillagerRenderer extends MobEntityRenderer<ZombieImpillagerEntity, ZombieImpillagerModel<ZombieImpillagerEntity>> {
 
@@ -26,10 +26,4 @@ public class ZombieImpillagerRenderer extends MobEntityRenderer<ZombieImpillager
         float g = 0.9375F * zombieImpillagerEntity.getScaleFactor();
         matrixStack.scale(g, g, g);
     }
-
-    /*@Override
-    protected float getShadowRadius(ZombieImpillagerEntity zombieImpillagerEntity) {
-        float f = super.getShadowRadius(zombieImpillagerEntity);
-        return zombieImpillagerEntity.isBaby() ? f * 0.5F : f;
-    }*/
 }
