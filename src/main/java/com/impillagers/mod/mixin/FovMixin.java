8ed483ce46a517601e9d97ea3e6a22aa20fde1cb
thomas-nurse <thomas.p.nurse@gmail.com>
2025-08-04 14:04:31 +0100

    Fixed #5 - Port FovMixin from a24769a close #5

--- src/main/java/com/impillagers/mod/mixin/FovMixin.java
+++ src/main/java/com/impillagers/mod/mixin/FovMixin.java
@@ -1,19 +1,16 @@
 package com.impillagers.mod.mixin;
 
+import com.impillagers.mod.Impillagers;
 import com.impillagers.mod.effect.ModEffectClient;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(GameRenderer.class)
 public class FovMixin {
-    @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
-    private void onGetFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> cir) {
-        double baseFov = cir.getReturnValue();
-        double modifiedFov = ModEffectClient.getAdjustedFOV(baseFov);
-        cir.setReturnValue(modifiedFov);
+    @ModifyVariable(method = "getFov", at = @At("STORE"), ordinal = 0)
+    private double modifyFov(double fov) {
+        return ModEffectClient.getAdjustedFOV(fov);
     }
 }
