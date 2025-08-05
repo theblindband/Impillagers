1b611b24011e70d4823a12d35ada023ba6acb808
thomas-nurse <thomas.p.nurse@gmail.com>
2025-08-05 09:54:56 +0100

    Removed wither check for belladonna

--- src/main/java/com/impillagers/mod/block/custom/BelladonnaBlock.java
+++ src/main/java/com/impillagers/mod/block/custom/BelladonnaBlock.java
@@ -51,10 +51,9 @@ public class BelladonnaBlock extends FlowerBlock {
     @Override
     protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
         if (!world.isClient && world.getDifficulty() != Difficulty.PEACEFUL) {
-            if (entity instanceof LivingEntity livingEntity && !livingEntity.isInvulnerableTo(world.getDamageSources().wither()) && !livingEntity.hasStatusEffect(StatusEffects.POISON)) {
+            if (entity instanceof LivingEntity livingEntity && !livingEntity.hasStatusEffect(StatusEffects.POISON)) {
                 livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 40, 1));
             }
         }
     }
 }
-
